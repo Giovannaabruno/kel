c778461832a8382d124682c14d3cfbc437ac0f61
kelsielam <kelsielam17@gmail.com>
2023-03-17 12:47:04 -0400

    added view tests

---
+++ test/View/ImageViewTest.java
@@ -0,0 +1,24 @@
+package View;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ImageViewTest {
+  Appendable out;
+  /**
+   * Test rendering message.
+   */
+  @Test
+  public void testRenderMessage() throws IOException {
+    out = new StringBuffer();
+    CollagingView view = new CollagingCommandView(out);
+    view.renderMessage("Testing Message!");
+    assertEquals("Testing Message!", out.toString());
+  }
+
+
+}
