ad0b9ab9f03dd30c8e7906d45633d81a6f0b0d2a
kelsielam <kelsielam17@gmail.com>
2023-03-16 23:23:29 -0400

    modified imports to run tests

--- test/Model/PixelTest.java
+++ test/Model/PixelTest.java
@@ -2,9 +2,10 @@ package Model;
 
 import org.junit.Test;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotEquals;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+
 
 public class PixelTest {
 
